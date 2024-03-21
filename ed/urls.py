18efd982be708a10a364376cad5aaad012e9bd80
Jamie Hogg <jamie-hogg@hotmail.com>
2024-03-21 12:11:20 +1000

    Before redesigning pages

--- ed/urls.py
+++ ed/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from ed import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("teams/", views.teams, name = "teams"),
+    path("teams_stochastic/", views.teams_stochastic, name = "ts")
 ]
